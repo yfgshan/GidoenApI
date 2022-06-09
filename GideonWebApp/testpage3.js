31edef1b1daa86355777b5883565eca9a60b2ae0
Gideon Shan <gideon.shan@activenetwork.com>
2022-06-09 17:28:46 +0800

    add two files for gitleaks testpag3.js

---
+++ GideonWebApp/testpage3.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('appAdmin.directives').directive('hlViewEditTaxConfig', HlViewEditTaxConfig);
+
+    function HlViewEditTaxConfig() {
+        return {
+            restrict: 'E',
+            templateUrl: 'script/viewEditTaxConfig/viewEditTaxConfig.html',
+            controller: ['$state', '_', '$stateParams', 'viewEditTaxConfigService',
+                function ($state, _, $stateParams, viewEditTaxConfigService) {
+                    this.clearData = _.bind(function (sourceObj) {
+                        for (var key in sourceObj) {
+                            if (key === 'submittedToIRSPriorYear' || key === "w9SStudentAccessURL") {
+                                continue;
+                            }
+                            sourceObj[key] = null;
+                        }
+                    }, this)
+
+                }
+            ],
+            bindToController: true,
+            controllerAs: 'x'
+        };
+    }
+})();
