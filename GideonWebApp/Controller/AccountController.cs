3a217703cbea89fdff31f88a4f1bffa998a06d49
Gideon Shan <gideon.shan@activenetwork.com>
2022-06-09 10:56:29 +0800

    test gitleaks

---
+++ GideonWebApp/Controller/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+//using RouteAttribute = System.Web.Http.RouteAttribute;
+
+namespace GideonWebApp.Controller
+{
+    [RoutePrefix("api/account")]
+    public class AccountController : ApiController
+    {
+        [Route("Login")]
+        [HttpGet]
+       public HttpResponseMessage Login()
+        {
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, "test");
+        }
+    }
+}
---
+++ GideonWebApp/DBContext/MyContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GideonWebApp.DBContext
+{
+
+    public class MyContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
+
